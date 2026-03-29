d21e3c4f20f298f615a47dd62481a98a9b3c4b68
Tan Thanh <thanhday247@gmail.com>
2026-03-29 10:57:53 +0700

    Add Monthly Active Users query (tik tok)

---
+++ datalemur/medium/Monthly-Active-Users.sql
@@ -0,0 +1,25 @@
+-- Problem: Monthly Active Users
+
+-- Description:
+-- Calculate the activation rate for activated emails.
+
+SELECT ROUND((1.0 * COUNT(t.email_id) 
+      / COUNT(DISTINCT e.email_id)),2) AS confirm_rate
+FROM emails AS e
+LEFT JOIN texts AS t 
+  ON e.email_id = t.email_id
+  AND t.signup_action = 'Confirmed'
+  
+  /*
+-- Explanation:
+-- 1. Use LEFT JOIN to merge the emails table (denoted as e) and the texts table 
+    (denoted as t) with the condition e.email_id = t.email_id and only retrieve 
+    emails containing the word "confirmed".
+      
+-- 2. Use COUNT to calculate the total number of activated emails and the total
+      number of emails, then divide the two totals.
+      
+-- 3. Multiply by 1.0 to convert to decimal.
+
+-- 4. Use ROUND to round to the second decimal place.
+*/
