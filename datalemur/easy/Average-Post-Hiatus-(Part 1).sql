4114151afb61da7283a370a09ab3b01395eda4c1
Tan Thanh <thanhday247@gmail.com>
2026-03-30 16:21:45 +0700

    Add Average Post Hiatus (Part 1) query (Facebook)

---
+++ datalemur/easy/Average-Post-Hiatus-(Part 1).sql
@@ -0,0 +1,26 @@
+-- Problem: Average Post Hiatus (Part 1)
+
+-- Description:
+-- Calculate the number of days between the first and last post for each user.
+-- Only include users who have published at least 2 posts.
+
+SELECT 
+  user_id,
+  EXTRACT(DAY FROM (MAX(post_date) - MIN(post_date))) AS days_between
+FROM posts
+GROUP BY user_id
+HAVING COUNT(post_id) > 1
+
+/*
+-- Explanation:
+-- 1. GROUP BY user_id to aggregate the posting data for each individual user.
+
+-- 2. Use the HAVING clause with COUNT(post_id) > 1 to filter out users who have 
+      only posted once.
+      
+-- 3. Calculate the time interval by subtracting the first post date (MIN) from 
+      the latest post date (MAX).
+      
+-- 4. Use EXTRACT(DAY FROM ...) to convert the resulting time interval into a 
+      standard integer representing the number of days.
+*/
