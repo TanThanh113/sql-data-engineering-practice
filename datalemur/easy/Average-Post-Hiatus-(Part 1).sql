132d701a42dcfd59e7fc5e5ad852e3c5cb786fd1
Tan Thanh <thanhday247@gmail.com>
2026-03-30 16:38:47 +0700

    Add Average Post Hiatus (Part 1) query (Facebook)

--- datalemur/easy/Average-Post-Hiatus-(Part 1).sql
+++ datalemur/easy/Average-Post-Hiatus-(Part 1).sql
@@ -8,6 +8,7 @@ SELECT
   user_id,
   EXTRACT(DAY FROM (MAX(post_date) - MIN(post_date))) AS days_between
 FROM posts
+WHERE EXTRACT (YEAR FROM post_date) = '2021'
 GROUP BY user_id
 HAVING COUNT(post_id) > 1
 
@@ -15,12 +16,14 @@ HAVING COUNT(post_id) > 1
 -- Explanation:
 -- 1. GROUP BY user_id to aggregate the posting data for each individual user.
 
--- 2. Use the HAVING clause with COUNT(post_id) > 1 to filter out users who have 
+-- 2. WHERE to filter post_dates in 2021.
+
+-- 3. Use the HAVING clause with COUNT(post_id) > 1 to filter out users who have 
       only posted once.
       
--- 3. Calculate the time interval by subtracting the first post date (MIN) from 
+-- 4. Calculate the time interval by subtracting the first post date (MIN) from 
       the latest post date (MAX).
       
--- 4. Use EXTRACT(DAY FROM ...) to convert the resulting time interval into a 
+-- 5. Use EXTRACT(DAY FROM ...) to convert the resulting time interval into a 
       standard integer representing the number of days.
 */
